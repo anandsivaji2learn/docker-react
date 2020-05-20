6b125ddde5c5af72125fa9a0802cd84c504d9e01
Anand Sivaji <anand.sivaji@cioxhealth.com>
2020-05-20 19:04:36 -0400

    Initial commit on the docker-react sample

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React through docker by compose.
         </a>
       </header>
     </div>
